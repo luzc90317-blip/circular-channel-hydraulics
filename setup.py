75f72c12311172abd2260cf8ce4b186ecc51afb1
luzc90317-blip <luzc90317@gmail.com>
2026-02-23 22:09:56 -0500

    Create setup.py for circular-hydraulics package

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py for circular-hydraulics package
+
+from setuptools import setup, find_packages
+
+setup(
+    name='circular-hydraulics',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A package for modeling circular channel hydraulics.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/luzc90317-blip/circular-channel-hydraulics',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        # Add your package dependencies here
+    ],
+)
